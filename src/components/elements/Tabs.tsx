b776beee2a61d66763873c973a19aaa6e6b3efda
unknown <amienulrana@gmail.com>
2024-02-12 01:57:32 +0700

    add tabs component

---
+++ src/components/elements/Tabs.tsx
@@ -0,0 +1,62 @@
+import { IPropsTabs } from "@/types";
+import { useEffect, useRef, useState } from "react";
+
+
+export default function Tabs({
+  tabActive,
+  setTabActive,
+  labels,
+  classNameWrapper,
+  defaultActive = 0,
+}: IPropsTabs) {
+  const [indicator, setIndicator] = useState({
+    width: 0,
+    position: 0,
+  });
+
+  const buttonRefs = useRef<HTMLButtonElement[]>([]);
+
+  const handleNavigateTabs = (event: any, tab: string) => {
+    const buttonWidth: number = event.target.clientWidth;
+    const buttonPosition: number = event.target.offsetLeft;
+    setIndicator({ width: buttonWidth, position: buttonPosition });
+    setTabActive(tab);
+  };
+  useEffect(() => {
+    setTabActive(labels[defaultActive]);
+  }, [labels]);
+
+  useEffect(() => {
+    // if (buttonRefs.current[defaultActive]) {
+    const buttonWidth = buttonRefs.current[defaultActive].clientWidth;
+    const buttonPosition = buttonRefs.current[defaultActive].offsetLeft;
+    setIndicator({ width: buttonWidth, position: buttonPosition });
+    // }
+  }, [labels]);
+  return (
+    <section className={`flex gap-5 min-w-max overflow-x-auto hide-scrollbar relative py-4 ${classNameWrapper}`}>
+      {tabActive && (
+        <span
+          className="absolute bottom-0 border-2 rounded-t-md border-primary duration-300"
+          style={{
+            left: `${indicator.position}px`,
+            width: `${indicator.width}px`,
+          }}
+        />
+      )}
+      {labels.map((data, index) => (
+        <button
+          key={index}
+          ref={(el) => (buttonRefs.current[index] = el!)}
+          className={[
+            "text-md mr-6 bg-transparent min-w-max duration-300 font-bold",
+            tabActive === data ? "text-primary" : "text-gray-300",
+          ].join(" ")}
+          onClick={(e) => handleNavigateTabs(e, data)}
+        >
+          {data}
+        </button>
+      ))}
+    </section>
+  );
+}
