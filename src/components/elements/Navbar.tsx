b22726f7538c5d6a752ddafd2c444271acf9c615
unknown <amienulrana@gmail.com>
2024-02-12 04:17:09 +0700

    remove sticky position in navbar

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="bg-darkgray sticky z-50 border-b-2 border-b-darkwhite top-0 pb-5">
+    <header className="border-b-2 border-b-darkwhite pb-5">
       <h1 className="text-3xl text-white">DoThat</h1>
     </header>
   );
