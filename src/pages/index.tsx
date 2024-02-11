442598d31f924d2f408715a8c54ebe5006932a12
unknown <amienulrana@gmail.com>
2024-02-12 01:47:35 +0700

    feat: render component add todo box in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter, Open_Sans, Poppins } from "next/font/google";
 import { Navbar } from "@/components/elements";
+import { AddTodoBox } from "@/components/pages/home";
 
 const inter = Poppins({ weight: "500", subsets: ["latin"] });
 
@@ -10,6 +11,9 @@ export default function Home() {
       className={`px-32 py-[2em] max-w-[1200px] mx-auto ${inter.className}`}
     >
       <Navbar />
+      <main className="sticky bg-[#121212] flex flex-col gap-[1em] z-[2] left-0 top-0">
+          <AddTodoBox />
+      </main>
     </section>
   );
 }
