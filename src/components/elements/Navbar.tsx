9a8ecc3fa00671bcbb3bdfa018c7b0c8d8d72a0e
unknown <amienulrana@gmail.com>
2024-02-12 00:17:26 +0700

    feat: create navbar elements

---
+++ src/components/elements/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header className="bg-darkgray sticky z-50 border-b-2 border-b-darkwhite top-0 pb-5">
+      <h1 className="text-3xl text-white">DoThat</h1>
+    </header>
+  );
+}
