67527f860437c874d6b09560a8ccb3320174264e
unknown <amienulrana@gmail.com>
2024-02-12 00:10:34 +0700

    create layout for the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Inter, Open_Sans, Poppins } from "next/font/google";
 
-const inter = Poppins({ weight: "500", subsets: ['latin'] });
+const inter = Poppins({ weight: "500", subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <section
+      className={`px-32 py-[2em] max-w-[1200px] mx-auto ${inter.className}`}
     >
       <h1>Hello</h1>
-    </main>
+    </section>
   );
 }
