90e9336dabee4b30294872e1e27c6f8094034582
unknown <amienulrana@gmail.com>
2024-02-12 01:54:23 +0700

    create function for handle edit, remove and complete todo

--- src/reducers/todoReducers.ts
+++ src/reducers/todoReducers.ts
@@ -15,7 +15,30 @@ import {
     return updatedTodoItemsList;
   }
   
- 
+  function removeTodoItem(state: TodoItem[], id: RemoveAction['payload']) {
+    return state.filter((todoItem) => todoItem.id !== id);
+  }
+  
+  function editTodoItem(
+    state: TodoItem[],
+    id: TodoId,
+    editedTodoItemCallback: (targetTodoItem: TodoItem) => Partial<BaseTodoItem>
+  ) {
+    return state.map((todoItem) => {
+      if (todoItem.id === id) {
+        const partialTodoItem = editedTodoItemCallback(todoItem);
+  
+        const updatedTodoItem = {
+          ...todoItem,
+          ...partialTodoItem,
+        };
+  
+        return updatedTodoItem;
+      }
+  
+      return todoItem;
+    });
+  }
   
   export default function reducer(
     state: TodoItem[],
@@ -25,6 +48,25 @@ import {
       case 'add':
         const todoItem = action.payload;
         return addTodoItem(state, todoItem);
+  
+      case 'remove':
+        const removeId = action.payload;
+        return removeTodoItem(state, removeId);
+  
+      case 'edit':
+        const editPayload = action.payload;
+        const editId = editPayload.id;
+  
+        return editTodoItem(state, editId, () => ({
+          todo: editPayload.todo
+        }));
+  
+      case 'toggleCompletion':
+        const toggleId = action.payload;
+  
+        return editTodoItem(state, toggleId, (todoItem) => ({
+          complete: !todoItem.complete,
+        }));
   
       default:
         return state;
